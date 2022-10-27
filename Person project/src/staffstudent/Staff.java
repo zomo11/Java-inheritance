ac446589351a382a031ed53b31a2e388e6138162
Moses <107884245+zomo11@users.noreply.github.com>
2022-10-27 17:51:41 +0200

    Add files via upload

---
+++ Person project/src/staffstudent/Staff.java
@@ -0,0 +1,33 @@
+package staffstudent;
+
+public class Staff extends Person{
+    private String school;
+    double pay;
+
+    public Staff(String name, String address, String school, double pay) {
+        super(name, address);
+        this.school = school;
+        this.pay = pay;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public double getPay() {
+        return pay;
+    }
+
+    public void setPay(double pay) {
+        this.pay = pay;
+    }
+
+    @Override
+    public String toString(){
+        return getName() + '\n' + getAddress() + '\n' + school + '\n' + pay;
+    }
+}
---
+++ Person project/src/staffstudent/testtest.java
@@ -0,0 +1,9 @@
+package staffstudent;
+
+public class testtest {
+    public static void main(String[] args) {
+
+        Student myStudent = new Student("Moses","Meldestraat","Computer",2012,5000);
+        System.out.println(myStudent.toString());
+    }
+}
